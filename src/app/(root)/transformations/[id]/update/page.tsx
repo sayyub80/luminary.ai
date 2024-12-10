a9334c82d28a20304fe63f5965b7d07c50ef05a5
sayyub80 <mdsayyub80@gmail.com>
2024-12-10 15:42:09 +0530

    update page added

--- src/app/(root)/transformations/[id]/update/page.tsx
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -1,7 +1,40 @@
-function Update() {
-    return ( 
-        <div>Update Transformation </div>
-    );
-}
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default Update;
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { getImageById } from "@/actions/image.actions";
+
+const Page = async ({ params: { id } }: SearchParamProps) => {
+    const { userId }: { userId: string | null } = await auth()
+
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
